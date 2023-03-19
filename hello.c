1e67a8799f8a0fc7dbaad3a157c066e1781f58c0
2231091023_SHAYKOT <128286512+shaykotselim2231091023@users.noreply.github.com>
2023-03-19 18:31:37 +0600

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,11 @@
+   #include<stdio.h>
+
+    int main(void){
+            printf("        T  H          E  L\n");
+            printf("       O    O        S    I\n");
+            printf("S     K      S      N      M\n");
+            printf(" H   Y        S    I         \n");
+            printf("   A            A");
+
+            getch();
+        };
